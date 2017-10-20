7b23413313d92c1fa105289da29a55dcbd8e606a
Diego <valenv.dg@gmail.com>
2017-10-20 16:07:59 -0600

    Primer Commit
    Trabajo de Clase

---
+++ src/lotificadora/Proyecto_Lotificacion.java
@@ -0,0 +1,56 @@
+
+package lotificadora;
+
+
+public class Proyecto_Lotificacion {
+    public static Terreno [][] lotes;
+
+    public Proyecto_Lotificacion() {
+        lotes = new Terreno[5][5];
+        
+    }
+    
+    public static boolean estaVacia(int a, int b ){
+      if(lotes[a][b].getOcupado() == false){
+          return true;
+      }else{
+          return false;
+      }
+    }
+    public static int terrOcupado(){
+        int cont1 = 0;
+
+        for (int i = 0; i < lotes.length; i++) {
+            for (int j = 0; j <lotes.length; j++) {
+                if(lotes[i][j].getOcupado() == true){
+                    cont1++;
+                    
+                }
+            }
+        }
+        return cont1;
+    }
+    
+    public static double totalLps(){
+        double total = 0;
+        for (int i = 0; i < lotes.length; i++) {
+            for (int j = 0; j < lotes.length; j++) {
+                total += lotes[i][j].getPrecioTotal();
+            }
+        }
+        return total;
+    }
+    public static int terrVacio(){
+        int cont2 = 0;
+        for (int i = 0; i < lotes.length; i++) {
+            for (int j = 0; j <lotes.length; j++) {
+                if(lotes[i][j].getOcupado()== false){
+                    cont2++;
+                    
+                }
+            }
+        }
+        return cont2;
+    }
+    
+}
--- src/lotificadora/Terreno.java
+++ src/lotificadora/Terreno.java
@@ -1,16 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lotificadora;
 
-/**
- *
- * @author Juan Zepeda
- */
 public class Terreno {
     private double largo, ancho, precio;
+    
+    private boolean ocupado;
+
+    public boolean getOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
 
     public Terreno(double largo, double ancho, double precio) {
         this.largo = largo;
